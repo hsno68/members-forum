06224040bdf24326d4096ffa19c99b1e1cce73d1
haiseno <haise.zhao@gmail.com>
2026-04-20 16:08:04 -0700

    Create signup form

--- controller.js
+++ controller.js
@@ -2,5 +2,5 @@ import * as db from "./db/queries.js";
 import bcrypt from "bcryptjs";
 
 export function getHomepage(req, res) {
-  res.render("layout", { title: "Home", page: "pages/homepage", css: null });
+  res.render("layout", { title: "Home", page: "pages/homepage", css: "/css/form.css" });
 }
